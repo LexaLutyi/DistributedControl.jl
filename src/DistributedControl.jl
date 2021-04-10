dba73912c038b626c0612232ef042bce466bec70
LexaLutyi <liotbiu1@gmail.com>
2021-04-10 12:21:22 +0300

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/DistributedControl.jl
@@ -0,0 +1,5 @@
+module DistributedControl
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DistributedControl
+using Test
+
+@testset "DistributedControl.jl" begin
+    # Write your tests here.
+end
